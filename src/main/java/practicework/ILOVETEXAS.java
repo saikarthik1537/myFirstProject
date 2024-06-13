356c440001778fb6b63d4b303bb4b6460e8ef40b
saikarthik1537 <141776291+saikarthik1537@users.noreply.github.com>
2024-06-13 18:38:23 -0500

    ILOVETEXAS working

--- src/main/java/practicework/ILOVETEXAS.java
+++ src/main/java/practicework/ILOVETEXAS.java
@@ -1,4 +1,18 @@
 package practicework;
 
 public class ILOVETEXAS {
+    public static void main(String[] args) {
+        String sentence = "I LOVE TEXAS";
+        char targetChar = 'A';
+        int count = 0;
+
+        for (int i = 0; i < sentence.length(); i++) {
+            if (sentence.charAt(i) == targetChar) {
+                count++;
+            }
+        }
+
+        System.out.println("The number of 'A' in the sentence 'I LOVE TEXAS' is: " + count);
+    }
 }
+
