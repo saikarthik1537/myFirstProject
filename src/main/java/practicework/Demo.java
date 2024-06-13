b593c2a492926b472e342c664cd1f43a957597e3
saikarthik1537 <141776291+saikarthik1537@users.noreply.github.com>
2024-06-13 18:18:24 -0500

    test1AllDone

---
+++ src/main/java/practicework/Demo.java
@@ -0,0 +1,13 @@
+package practicework;
+
+public class Demo {
+    public static void main(String a[])
+    {
+        int i =1;
+        while(true)
+        {
+            System.out.println("Hi" + i);
+            i++;
+        }
+    }
+}
